0a351e33c2bf0e1ee22e969eca0f9cbb3d5d66da
ronald sarmiento <ronisarmont@gmail.com>
2025-02-26 09:47:50 -0400

    delete and update on MovieModel created

--- clase-5/models/mysql/movie.js
+++ clase-5/models/mysql/movie.js
@@ -69,7 +69,40 @@ export class MovieModel {
     return movies[0];
   }
 
-  static async delete({ id }) {}
+  static async delete({ id }) {
+    try {
+      await connection.query('DELETE FROM movie WHERE id=UUID_TO_BIN(?)', [id]);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
 
-  static async update({ id, input }) {}
+  static async update({ id, input }) {
+    const {
+      genre: genreInput,
+      title,
+      year,
+      duration,
+      director,
+      rate,
+      poster,
+    } = input;
+
+    try {
+      await connection.query(
+        'UPDATE movie SET title=?, year=?, duration=?, director=?, rate=?, poster=? WHERE id=UUID_TO_BIN(?)',
+        [title, year, duration, director, rate, poster, id]
+      );
+    } catch (e) {
+      throw new Error('Error al actualizar la pelicula');
+    }
+
+    const [movies] = await connection.query(
+      `SELECT title, year, director, duration, poster, rate, BIN_TO_UUID(id) id FROM movie WHERE id=UUID_TO_BIN(?);`,
+      [id]
+    );
+
+    return movies[0];
+  }
 }
