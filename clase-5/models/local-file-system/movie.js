23c439f42b3f72066545ff39d1e4057cdb206e8b
ronald sarmiento <ronisarmont@gmail.com>
2025-02-28 10:54:08 -0400

    dependency injection

--- clase-5/models/local-file-system/movie.js
+++ clase-5/models/local-file-system/movie.js
@@ -1,52 +1,52 @@
-import { randomUUID } from 'node:crypto'
-import { readJSON } from '../../utils.js'
+import { randomUUID } from 'node:crypto';
+import { readJSON } from '../../utils.js';
 
-const movies = readJSON('./movies.json')
+const movies = readJSON('./movies.json');
 
 export class MovieModel {
-  static async getAll ({ genre }) {
+  static async getAll({ genre }) {
     if (genre) {
-      return movies.filter(
-        movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
-      )
+      return movies.filter((movie) =>
+        movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
+      );
     }
 
-    return movies
+    return movies;
   }
 
-  static async getById ({ id }) {
-    const movie = movies.find(movie => movie.id === id)
-    return movie
+  static async getById({ id }) {
+    const movie = movies.find((movie) => movie.id === id);
+    return movie;
   }
 
-  static async create ({ input }) {
+  static async create({ input }) {
     const newMovie = {
       id: randomUUID(),
-      ...input
-    }
+      ...input,
+    };
 
-    movies.push(newMovie)
+    movies.push(newMovie);
 
-    return newMovie
+    return newMovie;
   }
 
-  static async delete ({ id }) {
-    const movieIndex = movies.findIndex(movie => movie.id === id)
-    if (movieIndex === -1) return false
+  static async delete({ id }) {
+    const movieIndex = movies.findIndex((movie) => movie.id === id);
+    if (movieIndex === -1) return false;
 
-    movies.splice(movieIndex, 1)
-    return true
+    movies.splice(movieIndex, 1);
+    return true;
   }
 
-  static async update ({ id, input }) {
-    const movieIndex = movies.findIndex(movie => movie.id === id)
-    if (movieIndex === -1) return false
+  static async update({ id, input }) {
+    const movieIndex = movies.findIndex((movie) => movie.id === id);
+    if (movieIndex === -1) return false;
 
     movies[movieIndex] = {
       ...movies[movieIndex],
-      ...input
-    }
+      ...input,
+    };
 
-    return movies[movieIndex]
+    return movies[movieIndex];
   }
 }
